506500ce84ffe16fa13a439ed24c99bd8ce564b4
tariraf <tarisarafika2@gmail.com>
2022-03-23 20:19:12 +0700

    Second Commit
    Homework Module 2 Session 2

--- homeworkm2/src/App.js
+++ homeworkm2/src/App.js
@@ -2,25 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 import Songdesc from './Component/Songdesc';
 import Button from './Component/Button';
-import { albumImg } from './Component/Data';
+import { albumImg, title, artist } from './Component/Data';
 
 function App() {
-    return ( <
-        div className = "App" >
+    return ( < div className = "App" >
         <
         div className = 'song-detail' >
-        <
-        img src = { albumImg }
-        />
+
 
         <
-        Songdesc / >
+        Songdesc albumImg = { albumImg }
+        title = { title }
+        artist = { artist }
+        />
 
         <
         Button / >
 
         <
-        /div> <
+        /div>   <
         /div>
     );
 }
--- homeworkm2/src/Component/Songdesc.js
+++ homeworkm2/src/Component/Songdesc.js
@@ -1,10 +1,12 @@
 import '../App.css';
 import React from 'react';
-import { title, artist } from './Data';
-const Songdesc = () => {
+
+const Songdesc = ({ title, artist, albumImg }) => {
     return <div className = "song-desc" >
         <
-        h3 > { title } < /h3> <
+        img src = { albumImg }
+    /> <
+    h3 > { title } < /h3>  <
         p > < span className = 'by' > by < /span>{artist}</p >
         <
         /div>;
